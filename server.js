0153d9035b5f02e4bc67c0315a58bc11c1180343
Elliot Baynes <sbaynes1994@gmail.com>
2023-01-05 12:52:47 -0600

    move everything out of develop folder

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.static('public'));
 
 // homepage route
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 // notes route
 app.get('/notes', (req,res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html')))
+    res.sendFile(path.join(__dirname, 'public/notes.html')))
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
